90f7f15e1ac5acbafd74a11c39c01c8b99b135df
jeremybatesDC <jeremy.bates@gmail.com>
2022-01-08 12:54:17 -0500

    css reset per 2022 josh comeau

--- src/App.vue
+++ src/App.vue
@@ -34,16 +34,35 @@ export default {
 
 <style>
 
-* {
+/*
+  Josh's Custom CSS Reset
+  https://www.joshwcomeau.com/css/custom-css-reset/
+*/
+*, *::before, *::after {
   box-sizing: border-box;
 }
-
-body {
+* {
   margin: 0;
 }
-
-figure {
-  margin: 0;
+html, body {
+  height: 100%;
+}
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+input, button, textarea, select {
+  font: inherit;
+}
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+#root, #__next {
+  isolation: isolate;
 }
 
 label span {
